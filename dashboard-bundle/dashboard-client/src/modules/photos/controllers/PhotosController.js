ba070c98c8ce3b30f0b5411c9d7bb77441dfddc4
Mike Nimer <mnimer@gmail.com>
2013-12-05 17:32:20 -0600

    add showUpload toggle in photos for the side bar

--- dashboard-bundle/dashboard-client/src/modules/photos/controllers/PhotosController.js
+++ dashboard-bundle/dashboard-client/src/modules/photos/controllers/PhotosController.js
@@ -32,6 +32,23 @@
  *     along with the FamilyCloud Project.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+/*
+ * This file is part of FamilyCloud Project.
+ *
+ *     The FamilyCloud Project is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     The FamilyCloud Project is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with the FamilyCloud Project.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 var PhotosController = function($scope, $rootScope, $location, $modal, $state, photoService) {
 
     $scope.layout = "grid";
@@ -41,9 +58,8 @@ var PhotosController = function($scope, $rootScope, $location, $modal, $state, p
     // path to show in breadcrumb
     $scope.currentPath = "/";
     $scope.breadcrumb = [];
-    $scope.showSidebar = true;
-
-
+    $scope.showSidebar = false;
+    $scope.showUploadSidebar = false;
 
 
     $scope.refresh = function()
@@ -52,6 +68,19 @@ var PhotosController = function($scope, $rootScope, $location, $modal, $state, p
         $scope.$broadcast("refreshData");
     };
 
+
+    $scope.toggleUpload = function()
+    {
+        var b = !$scope.showUploadSidebar;
+        //show or hide the sidebar div, based on toggle
+        $scope.showSidebar = b;
+        $scope.showUploadSidebar = b;
+    };
+
+    $scope.isUploadVisible = function()
+    {
+        return $scope.showUploadSidebar;
+    };
 
     $scope.openCreateFolder = function()
     {
@@ -73,6 +102,7 @@ var PhotosController = function($scope, $rootScope, $location, $modal, $state, p
             }
         });
     };
+
 
 
 
@@ -129,6 +159,21 @@ var PhotosController = function($scope, $rootScope, $location, $modal, $state, p
         }
     };
     init();
+
+    /**
+     * Utility functions
+     */
+    $scope.safeApply = function(fn) {
+        var phase = this.$root.$$phase;
+        if(phase == '$apply' || phase == '$digest') {
+            if(fn && (typeof(fn) === 'function')) {
+                fn();
+            }
+        } else {
+            this.$apply(fn);
+        }
+    };
+
 };
 
 PhotosController.$inject = ['$scope', '$rootScope', '$location', '$modal', '$state', 'photoService'];
