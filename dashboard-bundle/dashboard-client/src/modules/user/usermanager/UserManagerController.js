f809fafe7e0e3e8394ab9b3cb3cdcda2fd91867e
Mike Nimer <mnimer@gmail.com>
2013-12-18 00:00:14 -0600

    stub in form for user manager, next step is to call sling api

--- dashboard-bundle/dashboard-client/src/modules/user/usermanager/UserManagerController.js
+++ dashboard-bundle/dashboard-client/src/modules/user/usermanager/UserManagerController.js
@@ -6,12 +6,41 @@
  */
 var UserManagerController = function($scope, $location) {
 
+    $scope.selectedUser = {};
+    $scope.users = [
+        {id:1, username:"Admin", firstName:"admin", lastName:""},
+        {id:2, username:"Mike", firstName:"Mike", lastName:"Nimer"},
+        {id:3, username:"Angie", firstName:"Angela", lastName:"Nimer"}
+    ];
+    $scope.currentUser = undefined;
+
+
+    $scope.selectUser = function()
+    {
+        //todo call api to get user based on selectedUser id
+
+        $scope.currentUser = $scope.selectedUser;
+
+        //set value to currentUser
+    };
+
+
+    $scope.createUser = function()
+    {
+        $scope.currentUser = {};
+    };
+
+
+    $scope.removeUser = function()
+    {
+
+    };
 
     /**
      * Invoked on startup, like a constructor.
      */
     var init = function(){
-
+        //todo: call sling to get users
     };
     init();
 };
