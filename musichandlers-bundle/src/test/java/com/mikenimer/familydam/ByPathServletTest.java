e033e6e1a7ce3cdf0e907e0e7bdd441f934273de
Mike Nimer <mnimer@gmail.com>
2014-02-06 19:48:29 -0600

    add the new copies of the filehandler-bundle (now imagehandler-bundle).
    Right now there is a lot of duplicate code. don't use as is.

---
+++ musichandlers-bundle/src/test/java/com/mikenimer/familydam/ByPathServletTest.java
@@ -0,0 +1,17 @@
+package com.mikenimer.familydam;
+
+import junit.framework.TestCase;
+
+/**
+ * Unit test for By Path Servlet.
+ */
+public class ByPathServletTest extends TestCase {
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testByPathServlet() {
+        assertTrue(true);
+    }
+    
+}
