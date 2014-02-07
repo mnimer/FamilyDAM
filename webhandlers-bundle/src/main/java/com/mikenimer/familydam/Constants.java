f10a8bc5c3be173a76cc7f5fd0e54463c7e6c6f1
Mike Nimer <mnimer@gmail.com>
2014-02-06 19:54:04 -0600

    delete the old files that were copied over and stub in the facebook archive job.

--- webhandlers-bundle/src/main/java/com/mikenimer/familydam/Constants.java
+++ webhandlers-bundle/src/main/java/com/mikenimer/familydam/Constants.java
@@ -22,55 +22,10 @@ import org.slf4j.LoggerFactory;
 
 /**
  * User: mikenimer
- * Date: 11/17/13
+ * Date: 02/06/14
  */
 public class Constants
 {
 
-    // JCR Paths
-    public static final String JOB_MOVE = "familydam/move";
-    public static final String JOB_MUSIC_METADATA = "familydam/music/metadata";
-    public static final String JOB_IMAGE_METADATA = "familydam/photos/metadata";
-    public static final String JOB_IMAGE_SIZE = "familydam/photos/size";
-    public static final String JOB_IMAGE_THUMBNAILS = "familydam/photos/thumbnails";
 
-    // Map keys
-    public static final String DATETIME = "dateTaken"; //todo change to datetime
-    public static final String ORIENTATION = "orientation"; //todo change to orientation
-    public static final String KEYWORDS = "keywords";
-    public static final String LATITUDE = "latitude";
-    public static final String LONGITUDE = "longitude";
-
-    //EXIF String
-    public static final String GPS = "GPS";
-    public static final String GPS_LATITUDE = "GPS Latitude";
-    public static final String GPS_LONGITUDE = "GPS Longitude";
-
-    // simple strings
-    public static final String PATH= "path";
-    public static final String NAME= "name";
-    public static final String TYPE= "type";
-    public static final String VALUE= "value";
-    public static final String DESCRIPTION= "description";
-    public static final String METADATA= "metadata";
-    public static final String TITLE = "title";
-    public static final String TRACK = "track";
-    public static final String ARTIST = "artist";
-    public static final String ALBUM = "album";
-    public static final String ALBUM_ARTIST = "album_artist";
-    public static final String ALBUM_IMAGE = "album_image";
-    public static final String ALBUM_IMAGE_MIMETYPE = "album_image_mimetype";
-    public static final String YEAR = "year";
-    public static final String GENRE = "genre";
-    public static final String GENRE_CODE = "genre_code";
-    public static final String COMMENT = "comment";
-    public static final String VERSION = "version";
-    public static final String CHAPTERS = "chapters";
-    public static final String CHAPTER_TOC = "chapters_toc";
-    public static final String COMPOSER = "composer";
-    public static final String COPYRIGHT = "copyright";
-    public static final String ENCODER = "encoder";
-    public static final String ITUNES_COMMENT = "itunes_comment";
-    public static final String ORIGINAL_ARTIST = "original_artist";
-    public static final String PUBLISHER = "publisher";
 }
--- webhandlers-bundle/src/main/java/com/mikenimer/familydam/filehandlers/jobs/web/FacebookArchiveJob.java
+++ webhandlers-bundle/src/main/java/com/mikenimer/familydam/filehandlers/jobs/web/FacebookArchiveJob.java
@@ -15,59 +15,72 @@
  *     along with the FamilyDAM Project.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-
-package com.mikenimer.familydam;
+package com.mikenimer.familydam.filehandlers.jobs.web;
 
 import org.apache.felix.scr.annotations.Activate;
 import org.apache.felix.scr.annotations.Component;
 import org.apache.felix.scr.annotations.Deactivate;
+import org.apache.felix.scr.annotations.Property;
 import org.apache.felix.scr.annotations.Reference;
+import org.apache.felix.scr.annotations.Service;
+import org.apache.sling.event.jobs.Job;
+import org.apache.sling.event.jobs.consumer.JobConsumer;
+import org.apache.sling.jcr.api.SlingRepository;
 import org.osgi.service.component.ComponentContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.jcr.Node;
 import javax.jcr.RepositoryException;
 import javax.jcr.Session;
 
 /**
- * User: mikenimer
- * Date: 11/25/13
+ * Created by mnimer on 2/6/14.
  */
-@Component(immediate = true)
-public class CustomNodeRegister
+@Component(enabled = true, immediate = true)
+@Service(value=JobConsumer.class)
+@Property(name= JobConsumer.PROPERTY_TOPICS, value="familydam/web/facebook/archive")
+public class FacebookArchiveJob implements JobConsumer
 {
-    private final Logger log = LoggerFactory.getLogger(CustomNodeRegister.class);
+
+    private final Logger log = LoggerFactory.getLogger(FacebookArchiveJob.class);
 
     @Reference
-    private Session session;
-
+    private SlingRepository repository;
 
     @Activate
     protected void activate(ComponentContext context) throws Exception
     {
-
+        log.debug("Activate ID3 Job");
     }
 
 
     @Deactivate
     protected void deactivate(ComponentContext componentContext) throws RepositoryException
     {
-        if (session != null)
+        log.debug("Deactivate ID3 Job");
+    }
+
+
+    @Override
+    public JobResult process(Job job)
+    {
+        String nodePath = (String)job.getProperty("nodePath");
+        return process(nodePath);
+    }
+
+
+    public JobResult process(String path)
+    {
+        try
         {
-            session.logout();
-            session = null;
+            Session session = repository.loginAdministrative(null);
+            Node node = session.getNode(path);
+            return JobResult.OK;
+        }catch( RepositoryException re ){
+            return JobResult.FAILED;
         }
     }
 
-    public static void RegisterCustomNodeTypes(Session session, String cndFileName)
-            throws Exception
-    {
-        // Get the JackrabbitNodeTypeManager from the Workspace.
-        // Note that it must be cast from the generic JCR NodeTypeManager to the
-        // Jackrabbit-specific implementation.
-        //NodeTypeManager manager = (NodeTypeManager) session.getWorkspace().getNodeTypeManager();
-        // Register the custom node types defined in the CND file
-        //manager.registerNodeTypes(new FileInputStream(cndFileName), NodeTypeManager.TEXT_X_JCR_CND);
-    }
 
 }
