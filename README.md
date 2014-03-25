485ea426c67d3d4e0d52c70a4b6b7fa579d194ef
Mike Nimer <mnimer@gmail.com>
2014-03-25 01:15:12 -0500

    start stubbing out the flickr api

--- README.md
+++ README.md
@@ -1,13 +1,8 @@
-# FamilyCloud
+# FamilyDAM
 A Digital Asset Manager (DAM) for families to run locally with everything stored on a local USB or NAS drive.
 Based on the same technologies as other enterprise digital asset manager (Apache Sling, Jackrabbit, and Felix)
 
-<a href="http://mnimer.github.io/FamilyDAM/">View website: http://mnimer.github.io/FamilyDAM/</a>
-
-"If it’s easier/faster to find something in 8 billion pages of the web than in your own 100 thousand files (including documents, emails, pictures, etc..), there’s a problem."<br/>
-- <a href="http://www.betaversion.org/~stefano/linotype/news/93/">Stefano Mazzocchi</a>
-- We are going to solve this problem!
-
+<a href="http://FamilyDAM.com">View website: http://FamilyDAM.com</a>
 
 
 ## Background
---
+++ bundles/webhandlers-bundle/src/main/java/com/mikenimer/familydam/web/jobs/flickr/FlickrPhotos.java
@@ -0,0 +1,88 @@
+/*
+ * This file is part of FamilyDAM Project.
+ *
+ *     The FamilyDAM Project is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     The FamilyDAM Project is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with the FamilyDAM Project.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.mikenimer.familydam.web.jobs.flickr;
+
+import com.mikenimer.familydam.web.jobs.facebook.fql.FacebookFqlJob;
+import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.Property;
+import org.apache.felix.scr.annotations.Reference;
+import org.apache.felix.scr.annotations.Service;
+import org.apache.sling.event.jobs.Job;
+import org.apache.sling.event.jobs.JobManager;
+import org.apache.sling.event.jobs.consumer.JobConsumer;
+import org.apache.sling.jcr.api.SlingRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.jcr.Node;
+import javax.jcr.Session;
+
+
+@Component(enabled = true, immediate = true)
+@Service(value = JobConsumer.class)
+@Property(name = JobConsumer.PROPERTY_TOPICS, value = "familydam/web/flickr/photos")
+public class FlickrPhotos implements JobConsumer
+{
+    public static String TOPIC = "familydam/web/flickr/photos";
+    public static String FOLDERPATH = "/content/dam/web/flickr/{1}/photos/{2}";
+    public static String NODEPATH = "/content/dam/web/flickr/{1}/photos/{2}/{3}";
+
+    private final Logger log = LoggerFactory.getLogger(FlickrPhotos.class);
+    private Session session;
+
+    @Reference
+    protected JobManager jobManager;
+
+
+    @Reference
+    public SlingRepository repository;
+
+
+    @Override
+    public JobResult process(Job job)
+    {
+        String userNode = (String) job.getProperty("userNode");
+
+
+        try
+        {
+            session = repository.loginAdministrative(null);
+            Node node = session.getNode(userNode);
+
+            Node flickrData = node.getNode("web/flickr");
+            if (flickrData != null)
+            {
+                return queryFlickr(job, flickrData);
+            }
+            // No Data, but job was still invoked
+            return JobResult.FAILED;
+        }
+        catch (Exception re)
+        {
+            return JobResult.CANCEL;
+        }
+    }
+
+
+    private JobResult queryFlickr(Job job, Node flickrData, String username)
+    {
+        String username = (String) job.getProperty("username");
+
+        return JobResult.OK;
+    }
+}
