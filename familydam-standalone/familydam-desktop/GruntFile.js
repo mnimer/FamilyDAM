6898171e596f7a1b0e9a8537e579e5113159f0ae
Mike Nimer <mikenimer@yahoo.com>
2014-05-27 00:06:55 -0500

    rename familydam-standalone to familydam-server
    add a new project that uses the Atom-Shell project to create a desktop application out of the familydam.

---
+++ familydam-standalone/familydam-desktop/GruntFile.js
@@ -0,0 +1,132 @@
+/*
+ * This file is part of FamilyDAM Project.
+ *
+ *     The FamilyDAM Project is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     The FamilyDAM Project is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with the FamilyDAM Project.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+module.exports = function (grunt) {
+
+	var os = require("os");
+
+    // load all grunt tasks
+    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+    // some other cool plugins to look at...
+	// https://npmjs.org/package/grunt-phonegap-build
+	// https://npmjs.org/package/grunt-s3
+
+	/* grunt: build a develop dist
+	 * grunt: release: build a release dist
+	 * grunt server: start server and build a develop dist any time a file changes
+	 */
+
+	// default task
+	grunt.registerTask('default', ['clean', 'build']);
+
+	// build tasks
+	grunt.registerTask('build', ['copy', 'build-atom-shell-app']);
+
+
+	grunt.initConfig({
+
+		// ========================================
+		// Common
+		// ========================================
+
+		// config
+		assetsVersion: 1, // increment each release
+		distdir: 'dist',
+		tempdir: '.temp',
+		pkg: grunt.file.readJSON('package.json'),
+        'bower-install': {
+            target: {
+                html: '<%= src %>/index.html',
+                ignorePath: '<%= src %>/'
+            }
+        },
+        banner:
+			'/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %>\n' +
+				'<%= pkg.homepage ? " * " + pkg.homepage + "\\n" : "" %>' +
+				' * Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author %>;\n' +
+				' * Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %>\n */\n',
+		src: {
+			js: ['src/**/*.js'],
+			html: ['src/**/*.tpl.html']
+		},
+
+
+		// clean
+		clean: ['./dist/*', '<%= tempdir %>/*'],
+
+        // copy
+        copy: {
+            assets: {
+                files: [
+                    {
+                        cwd: 'src/assets/',
+                        src: '**',
+                        dest: './dist/assets/',
+                        expand: true
+                    }
+                ]
+            },
+            html: {
+                files: [
+                    {
+                        cwd: 'src/',
+                        src: '*.html',
+                        dest: './dist/',
+                        expand: true
+                    }
+                ]
+            },
+            js: {
+                files: [
+                    {
+                        cwd: 'src/',
+                        src: '*.js',
+                        dest: './dist/',
+                        expand: true
+                    }
+                ]
+            },
+            json: {
+                files: [
+                    {
+                        cwd: 'src/',
+                        src: '*.json',
+                        dest: './dist/',
+                        expand: true
+                    }
+                ]
+            }
+        },
+
+        'download-atom-shell': {
+            version: '0.12.3',
+            outputDir: 'binaries'
+        },
+
+        'build-atom-shell-app': {
+            options: {
+                platforms: ["darwin", "win32", "linux"],
+                app_dir:"dist",
+                cache_dir:"binaries",
+                build_dir:"binary-dist"
+            }
+        }
+
+	});
+
+};
---
+++ familydam-standalone/familydam-desktop/src/main.js
@@ -0,0 +1,51 @@
+/*
+ * This file is part of FamilyDAM Project.
+ *
+ *     The FamilyDAM Project is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     The FamilyDAM Project is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with the FamilyDAM Project.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+var app = require('app');  // Module to control application life.
+var BrowserWindow = require('browser-window');  // Module to create native browser window.
+
+// Report crashes to our server.
+require('crash-reporter').start();
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the javascript object is GCed.
+var mainWindow = null;
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function() {
+    if (process.platform != 'darwin')
+        app.quit();
+});
+
+// This method will be called when atom-shell has done everything
+// initialization and ready for creating browser windows.
+app.on('ready', function() {
+    // Create the browser window.
+    mainWindow = new BrowserWindow({width: 1024, height: 800});
+
+    // and load the index.html of the app.
+    //mainWindow.loadUrl('file://' + __dirname + '/index.html');
+    mainWindow.loadUrl('http://localhost:8080');
+
+    // Emitted when the window is closed.
+    mainWindow.on('closed', function() {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        mainWindow = null;
+    });
+});
